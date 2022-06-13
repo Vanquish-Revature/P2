b0b7b1f01d71fade0111c162f2d8865bd681a9bb
Cody Taylor Jenkins <codytaylorjenkins@Codys-MBP.attlocal.net>
2022-06-13 08:20:36 -0500

    front end products page, and assets

---
+++ Project2/FRONTEND/angular-project-2/src/app/service/plant-service/plant.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Plant } from 'src/app/plant';
+import { Plants } from 'src/app/mock-plants';
+import { MessageService } from '../message/message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantService {
+
+  constructor(private messageService: MessageService) { }
+
+  getPlants(): Observable<Plant[]> {
+    const plants = of(Plants);
+    this.messageService.add('PlantService: fetched plants');
+    return plants;
+  }
+
+  getPlant(id: number): Observable<Plant> {
+    const plant = Plants.find(p => p.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(plant);
+}
+}
