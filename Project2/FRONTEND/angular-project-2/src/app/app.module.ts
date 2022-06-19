2298ec61b0ca26ad72c0b98497a784054db1dd89
Kevin Ngo <kevin.ngo2@mavs.uta.edu>
2022-06-18 23:20:29 -0500

    pushing updated changes

--- Project2/FRONTEND/angular-project-2/src/app/app.module.ts
+++ Project2/FRONTEND/angular-project-2/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
