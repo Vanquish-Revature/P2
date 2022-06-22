9a203983bc190a64c011d81ed42a95c621662bca
christine nwonuala <chrisnwogu5@gmail.com>
2022-06-22 01:00:56 -0400

    updated

--- Project2/src/main/java/com/revature/repo/CartDAO.java
+++ Project2/src/main/java/com/revature/repo/CartDAO.java
@@ -6,15 +6,30 @@ import com.revature.models.User;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CartDAO extends JpaRepository<Cart, Integer> {
-	
-	//Need to implement this
-	public List<Cart> findCartItemsByUser(User user);
 
-	public Cart save(Cart cart);
-	
-	
+
+import org.hibernate.Session;
+import com.revature.utilities.HibernateUtil;
+public class CartDAO {
+
+public int submitCart(Cart cart) {
+Session ses = HibernateUtil.getSession();
+ses.save(cart);
+HibernateUtil.closeSession();
+return 0;
+}
+
+public List<Cart> getAllCarts() {
+Session ses = HibernateUtil.getSession();
+List<Cart> cartList = ses.createQuery("FROM Cart").list();
+HibernateUtil.closeSession();
+return cartList;
+}
+
+public Cart getCartById(int id) {
+Session ses = HibernateUtil.getSession();
+Cart cart = ses.get(Cart.class, id);
+HibernateUtil.closeSession();
+return cart;
+}
 }
